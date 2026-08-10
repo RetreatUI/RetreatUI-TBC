c4ecf2e7d83a3276bca96cfdd3b9b4d9f53d1e15
Retreat <gaarn94@gmail.com>
2026-08-10 14:52:18 +0200

    Bump TBC beta version to 0.1.0-beta.9

--- RetreatUI/Core.lua
+++ RetreatUI/Core.lua
@@ -4,7 +4,7 @@ RetreatUITBC = RetreatUITBC or {}
 local RUI = RetreatUITBC
 
 RUI.name = addonName
-RUI.version = "0.1.0-beta.8"
+RUI.version = "0.1.0-beta.9"
 RUI.modules = RUI.modules or {}
 RUI.profilePayloads = RUI.profilePayloads or {}
 RUI.weakAuraPackages = RUI.weakAuraPackages or {}
